de7aa03ae3e811f7cfd79863e8be91f44c26944d
oi <oifuwe@luc.edu>
2022-03-23 19:22:27 -0500

    add spring security fundamentals

---
+++ spring-security-fundamentals/02-the-fundamentals/firstapp/src/main/java/com/oi/firstapp/MySecurityFilter.java
@@ -0,0 +1,14 @@
+package com.oi.firstapp;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MySecurityFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    System.out.println("Before");
+    chain.doFilter(request, response);
+    System.out.println("After");
+  }
+}
