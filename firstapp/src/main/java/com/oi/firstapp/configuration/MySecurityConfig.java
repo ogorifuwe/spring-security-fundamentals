933a3a3e43ceaec1ee6b58c8a54e3d70008a17fd
oi <oifuwe@luc.edu>
2023-07-25 23:52:28 -0500

    add custom basic security config

---
+++ firstapp/src/main/java/com/oi/firstapp/configuration/MySecurityConfig.java
@@ -0,0 +1,37 @@
+package com.oi.firstapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class MySecurityConfig {
+
+  @Bean
+  UserDetailsService userDetailsService() {
+    InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+    UserDetails user = User.withUsername("tom").password(passwordEncoder().encode("cruise")).authorities("read").build();7
+    userDetailsService.createUser(user);
+    return userDetailsService;
+  }
+
+  @Bean
+  BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.httpBasic();
+    httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+    return httpSecurity.build();
+  }
+
+}
